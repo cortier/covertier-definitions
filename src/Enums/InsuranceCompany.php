181c72d88c8ee544fcabbd1d73f433c4f7de632a
alexander villalobos <darksihart19@gmail.com>
2024-04-05 14:00:45 -0400

    add new Enums data

---
+++ src/Enums/InsuranceCompany.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Covertier\Catalogs\Enums;
+enum InsuranceCompany: string
+{
+    case QUALITAS = 'QUALITAS';
+}
---
+++ src/Enums/PolicyType.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Covertier\Catalogs\Enums;
+
+enum PolicyType: string
+{
+    case MX_TOURIST= 'MX_TOURIST';
+}
