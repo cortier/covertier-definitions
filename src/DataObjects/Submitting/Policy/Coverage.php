0ce1ecb917fe2696c954841e3e3f85744302038d
alexander villalobos <darksihart19@gmail.com>
2024-04-23 18:08:16 -0400

    agregar name y shortName a la clase submit de coverage

--- src/DataObjects/Submitting/Policy/Coverage.php
+++ src/DataObjects/Submitting/Policy/Coverage.php
@@ -8,6 +8,8 @@ class Coverage
 {
     public function __construct(
         public int $id,
+        public string $name,
+        public string $shortName,
         public float $insuredSum,
         public int $sumType,
         public float $deductible,
