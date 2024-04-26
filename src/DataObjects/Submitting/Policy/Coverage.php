02a3f088e9ec188ffac8d1c14087a0dbee08006c
alexander villalobos <darksihart19@gmail.com>
2024-04-26 19:43:30 -0400

    corrigiendo el submit de coverage

--- src/DataObjects/Submitting/Policy/Coverage.php
+++ src/DataObjects/Submitting/Policy/Coverage.php
@@ -8,12 +8,7 @@ class Coverage
 {
     public function __construct(
         public int $id,
-        public string $name,
-        public string $nameEn,
-        public string $shortName,
         public int $sumType,
-        public bool $isAddOn,
-        public bool $isExcludable,
         public ?float $insuredSum = null,
         public ?float $deductible = null
     ) {
