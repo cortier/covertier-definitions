af0e207c58c85f88f83694190e99b0e2f76c7977
alexander villalobos <darksihart19@gmail.com>
2024-04-30 17:20:34 -0400

    colocar las propiedades dentro de construc como privadas

--- src/DataObjects/Fetching/Policy/Coverage.php
+++ src/DataObjects/Fetching/Policy/Coverage.php
@@ -6,24 +6,40 @@ namespace Covertier\Definitions\DataObjects\Fetching\Policy;
 
 readonly class Coverage
 {
+
+
     /**
      * @param array<int, int>|null $insuredSumOptions
      * @param array<int, int>|null $sumTypeOptions
      * @param array<int, float>|null $deductibleOptions
      */
     public function __construct(
-        public int $id,
+        public int    $id,
         public string $shortName,
         public string $name,
         public string $nameEn,
-        public bool $isAddOn,
-        public bool $isExcludable,
+        public bool   $isAddOn,
+        public bool   $isExcludable,
         public ?float $premium,
-        public ?int $sumType,
+        public ?int   $sumType,
         public ?float $insuredSum,
         public ?float $deductible,
         public ?array $insuredSumOptions,
         public ?array $sumTypeOptions,
         public ?array $deductibleOptions,
-    ) {}
+        private ?bool $softForce,
+        private ?bool $isRestricted,
+    )
+    {
+    }
+
+    public function getSoftForce(): bool|null
+    {
+        return $this->softForce;
+    }
+
+    public function getIsRectricted(): bool|null
+    {
+        return $this->isRestricted;
+    }
 }
--- src/DataObjects/Submitting/Policy/Coverage.php
+++ src/DataObjects/Submitting/Policy/Coverage.php
@@ -6,8 +6,6 @@ namespace Covertier\Definitions\DataObjects\Submitting\Policy;
 
 class Coverage
 {
-    private ?bool $softForce;
-    private ?bool $isRestricted;
     public function __construct(
         public int $id,
         public int $sumType,
@@ -17,31 +15,5 @@ class Coverage
     ) {
     }
 
-    /**
-     * @param bool $softForce
-     * @return void
-     */
-    public function setSoftForce(bool $softForce): void
-    {
-        $this->softForce = $softForce;
-    }
 
-    public function getSoftForce(): bool|null
-    {
-        return $this->softForce;
-    }
-    
-    /**
-     * @param bool $isRestricted
-     * @return void
-     */
-    public function setIsRectricted(bool $isRestricted): void
-    {
-        $this->isRestricted = $isRestricted;
-    }
-
-    public function getIsRectricted(): bool|null
-    {
-        return $this->isRestricted;
-    }
 }
