b43583d8f599f295a24362bd765d1efeb26ad0ef
alexander villalobos <darksihart19@gmail.com>
2024-04-30 13:49:23 -0400

    colocar exclude enl submit de coverage

--- src/DataObjects/Submitting/Policy/Coverage.php
+++ src/DataObjects/Submitting/Policy/Coverage.php
@@ -9,6 +9,7 @@ class Coverage
     public function __construct(
         public int $id,
         public int $sumType,
+        public bool $exclude = false,
         public ?float $insuredSum = null,
         public ?float $deductible = null
     ) {
