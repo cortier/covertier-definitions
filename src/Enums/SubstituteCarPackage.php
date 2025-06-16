9e69a2e8b11c6de708b83dd91df87892f7016325
Roberto F <roberto@cortier.com>
2025-06-16 13:01:23 -0700

    feat(enum): Adding SubstituteCarPackage.php

---
+++ src/Enums/SubstituteCarPackage.php
@@ -0,0 +1,11 @@
+<?php
+declare(strict_types=1);
+
+namespace Covertier\Definitions\Enums;
+enum SubstituteCarPackage: int
+{
+    /* Natural Person */
+    case PKG_250 = 1;
+    case PKG_500 = 2;
+    case PKG_750 = 3;
+}
