bb0400b95315693d89d6b06dc9474da27fc63a3c
Roberto F <roberto@cortier.com>
2025-06-16 12:41:58 -0700

    refactor(data-objects): Coverage sumType is now nullable, since only a few coverages really required this property.

--- src/DataObjects/Submitting/Policy/Coverage.php
+++ src/DataObjects/Submitting/Policy/Coverage.php
@@ -8,12 +8,9 @@ class Coverage
 {
     public function __construct(
         public int $id,
-        public int $sumType,
         public bool $exclude = false,
+        public ?int $sumType = null,
         public ?float $insuredSum = null,
         public ?float $deductible = null
-    ) {
-    }
-
-
+    ) {}
 }
